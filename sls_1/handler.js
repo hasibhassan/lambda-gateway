d0afdb4c799b7f84ff86e207ed9f1d69768e69ac
hqhas <67892792+hqhas@users.noreply.github.com>
2021-03-09 00:59:24 -0600

    refactor:

--- sls_1/handler.js
+++ sls_1/handler.js
@@ -1,11 +1,9 @@
-'use strict'
-
-module.exports.hello = async (event) => {
+export async function hello(event) {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'hello from sls_1 framework v3!',
+        message: 'hello from sls_1 framework v4!',
         input: event,
       },
       null,
