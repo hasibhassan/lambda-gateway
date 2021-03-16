6bccda47e9172e2bccc8a72c718585ccaf36be69
hqhas <67892792+hqhas@users.noreply.github.com>
2021-03-16 03:25:59 -0500

    perf:

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
@@ -10,6 +12,7 @@ module.exports = {
   performance: {
     hints: false,
   },
+  externals: [nodeExternals(), 'dd-trace', 'datadog-lambda-js'],
   module: {
     rules: [
       {
